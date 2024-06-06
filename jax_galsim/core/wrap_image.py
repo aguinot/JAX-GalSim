6a8de4b8d2babad13825d82c603e05408b9c8b12
aguinot <axel.guinot.astro@gmail.com>
2024-06-06 19:12:56 -0400

    New version of faster wrapping

--- jax_galsim/core/wrap_image.py
+++ jax_galsim/core/wrap_image.py
@@ -4,37 +4,37 @@ import jax.numpy as jnp
 
 
 @jax.jit
+def _body_j(j, vals):
+    i, im, xmin, ymin, nxwrap, nywrap = vals
+
+    ii = (i - ymin) % nywrap + ymin
+    jj = (j - xmin) % nxwrap + xmin
+
+    im = jax.lax.cond(
+        # weird way to say if ii != i and jj != j
+        # I tried other ways and got test failures
+        jnp.abs(ii - i) + jnp.abs(jj - j) != 0,
+        lambda im, i, j, ii, jj: im.at[ii, jj].add(im[i, j]),
+        lambda im, i, j, ii, jj: im,
+        im,
+        i,
+        j,
+        ii,
+        jj,
+    )
+
+    return [i, im]
+
+@jax.jit
+def _body_i(i, vals):
+    im, xmin, ymin, nxwrap, nywrap = vals
+    _, im = jax.lax.fori_loop(0, im.shape[1], _body_j, [i, im, xmin, ymin, nxwrap, nywrap])
+    return im
+
+@jax.jit
 def wrap_nonhermitian(im, xmin, ymin, nxwrap, nywrap):
 
-    @jax.jit
-    def _body_j(j, vals):
-        i, im = vals
-
-        ii = (i - ymin) % nywrap + ymin
-        jj = (j - xmin) % nxwrap + xmin
-
-        im = jax.lax.cond(
-            # weird way to say if ii != i and jj != j
-            # I tried other ways and got test failures
-            jnp.abs(ii - i) + jnp.abs(jj - j) != 0,
-            lambda im, i, j, ii, jj: im.at[ii, jj].add(im[i, j]),
-            lambda im, i, j, ii, jj: im,
-            im,
-            i,
-            j,
-            ii,
-            jj,
-        )
-
-        return [i, im]
-
-    @jax.jit
-    def _body_i(i, vals):
-        im = vals
-        _, im = jax.lax.fori_loop(0, im.shape[1], _body_j, [i, im])
-        return im
-
-    im = jax.lax.fori_loop(0, im.shape[0], _body_i, im)
+    im = jax.lax.fori_loop(0, im.shape[0], _body_i, [im, xmin, ymin, nxwrap, nywrap])
     return im
 
 
